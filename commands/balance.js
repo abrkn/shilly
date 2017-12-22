7477c5a8a9a0ef22074bfb17d14bafa2b7eb2e44
Andreas Brekken <andreas@brekken.com>
2017-12-22 23:25:18 +0100

    Add nominal usd value to balance

--- commands/balance.js
+++ commands/balance.js
@@ -1,5 +1,5 @@
-const { fetchTotalTetherTokens } = require('../apis');
-const { formatBch } = require('../utils');
+const { fetchTotalTetherTokens, fetchCoinmarketcap } = require('../apis');
+const { formatBch, n } = require('../utils');
 const numeral = require('numeral');
 
 module.exports = async ({ recipient, message, reply, params, tipping, isDm }) => {
@@ -7,6 +7,10 @@ module.exports = async ({ recipient, message, reply, params, tipping, isDm }) =>
     return;
   }
 
+  const usdRate = (await fetchCoinmarketcap('bitcoin-cash')).price_usd;
   const balance = await tipping.getBalanceForUser(recipient.id);
-  await reply(`Balance: \`${formatBch(balance)}\``);
+
+  const asUsd = numeral(n(balance).mul(usdRate).toString()).format('0,0.000');
+
+  await reply(`Balance: \`${formatBch(balance)}\` (\`$${asUsd}\`)`);
 };
